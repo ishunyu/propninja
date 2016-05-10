be576344d112782e190392356c0a7e56d3dafbc8
shun.yu <shun.yu@workday.com>
2016-05-10 12:09:20 +0200

    Fix log level to INFO

--- propninja/utils/PNLoggerUtils.h
+++ propninja/utils/PNLoggerUtils.h
@@ -12,9 +12,9 @@
 #import <CocoaLumberjack/DDTTYLogger.h>
 #import <CocoaLumberjack/DDFileLogger.h>
 
-// static const int ddLogLevel = LOG_LEVEL_INFO;
+static const int ddLogLevel = LOG_LEVEL_INFO;
 // static const int ddLogLevel = LOG_LEVEL_DEBUG;
-static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+// static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
 @interface PNLoggerUtils : NSObject
 
